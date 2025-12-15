8b20785eb9d8f332ea5e39b6224595031a840cde
Caco <jiossandon@uc.cl>
2025-12-15 09:07:56 -0800

    style: update aspect ratio class for video area in Demo component

--- site/src/components/demo.tsx
+++ site/src/components/demo.tsx
@@ -4,7 +4,7 @@ export function Demo() {
       {/* Simulated macOS Window */}
       <div className="relative rounded-lg overflow-hidden border border-white/10 shadow-lg bg-black">
         {/* Video Area - aspect ratio prevents layout shift while loading */}
-        <div className="relative aspect-video">
+        <div className="relative aspect-1022/663">
           <video
             className="absolute inset-0 w-full h-full object-cover"
             src="/demo.mov"
